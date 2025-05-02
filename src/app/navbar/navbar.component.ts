13de50be6baefa16c3d45e2729d0cfbe616cb5ad
Orzhov <alouis@stagiaire-humanbooster.com>
2025-05-02 16:03:44 +0200

    rendu exam Web Statique

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink, RouterLinkActive,CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  
+  isOpen: boolean = false;
+
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
+}
